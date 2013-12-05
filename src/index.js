95fcdf321114338b0051bdf185b670c097c69bb2
Simon Lindsay <simon.lindsay@f-i.com>
2013-12-05 16:43:14 -0500

    added extra tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /**@license
- * mout v0.8.1 | http://moutjs.com | MIT license
+ * mout v0.8.2 | http://moutjs.com | MIT license
  */
 define(function(require){
 
 //automatically generated, do not edit!
 //run `node build` instead
 return {
-    'VERSION' : '0.8.1',
+    'VERSION' : '0.8.2',
     'array' : require('./array'),
     'collection' : require('./collection'),
     'date' : require('./date'),
